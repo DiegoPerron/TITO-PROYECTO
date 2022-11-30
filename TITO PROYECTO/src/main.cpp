83aa03957aaa9cc5ea02a186f49a60ae16700ec6
DiegoPerron <2020348121@uteq.edu.mx>
2022-11-30 15:48:24 -0600

    arreglao por ahora,falta ultrasonico

--- TITO PROYECTO/src/main.cpp
+++ TITO PROYECTO/src/main.cpp
@@ -1,31 +1,36 @@
 #include <Arduino.h>
 #include <HX711.h>
 
-const byte hx711_data_pin = A1;
-const byte hx711_clock_pin = A0;
-const byte bombpwm1 = 3;
-const byte bombpwm2 = 5;
-char caso;
+const byte hx711_data_pin = A1;  // DOUT
+const byte hx711_clock_pin = A0;    // CLK
+const byte bombpwm1 = 3;  // PWM1
+const byte bombpwm2 = 5;  // PWM2
+HX711 scale;  // Create an instance of the HX711 class
+char caso;  // Variable para el switch
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600);  // Start serial communication
   // Mandamos comandos para toma de temperatura a los sensores
-  scale.begin(hx711_data_pin, hx711_clock_pin);
-  pinMode(bombpwm1, OUTPUT);
-  pinMode(bombpwm2, OUTPUT);
+  scale.begin(hx711_data_pin, hx711_clock_pin);  // Initialize the HX711 library
+  scale.set_scale(439430.25);  // Set the scale factor
+  Serial.println(scale.read());   // Read the current value
+  scale.tare(0);  // Tare the scale
+  Serial.println(scale.get_units(20),2);  // Read the current value in grams
+  pinMode(bombpwm1, OUTPUT);  // Set the PWM1 pin as an output
+  pinMode(bombpwm2, OUTPUT);  // Set the PWM2 pin as an output
+  analogWrite(bombpwm1, 63);  // Set the PWM1 pin to 63
+  analogWrite(bombpwm2, 63);    // Set the PWM2 pin to 63
 }
 
 void loop()
 {
-
-  // Leemos y mostramos los datos de los sensores DS18B20
+  String T1 = String(scale.get_units(20),2);    // obtener t1P
   Serial.println(scale.read()); // obtener t1
   String T2 = String("23");                     // obtener t2P
-  String T2 = String("23");  
 
   Serial.print(T1 + "A" + T2);
-  delay(100);
+  delay(1000);
 
   if (Serial.available())
   {

aa4449881b99e4d0783e5b9ff62a3be49b7a0203
DiegoPerron <2020348121@uteq.edu.mx>
2022-11-30 16:02:32 -0600

    ARREGLAO PERO FALTA PROBAR PERO COMPILA

--- TITO PROYECTO/src/main.cpp
+++ TITO PROYECTO/src/main.cpp
@@ -2,11 +2,11 @@
 #include <HX711.h>
 
 const byte hx711_data_pin = A1;  // DOUT
-const byte hx711_clock_pin = A0;    // CLK
-const byte bombpwm1 = 3;  // PWM1
-const byte bombpwm2 = 5;  // PWM2
-HX711 scale;  // Create an instance of the HX711 class
-char caso;  // Variable para el switch
+const byte hx711_clock_pin = A0; // CLK
+const byte bombpwm1 = 3;         // PWM1
+const byte bombpwm2 = 5;         // PWM2
+HX711 scale;                     // Create an instance of the HX711 class
+char caso;                       // Variable para el switch
 // Configuramos los pines del sensor Trigger y Echo
 const int PinTrig = 12;
 const int PinEcho = 11;
@@ -15,26 +15,26 @@ const float VelSon = 34000.0;
 // Número de muestras
 const int numLecturas = 20;
 // Distancia a los 100 ml y vacío
-const float distanciaVacio = 30; 
+const float distanciaVacio = 30;
 float lecturas[numLecturas]; // Array para almacenar lecturas
-int lecturaActual = 0; // Lectura por la que vamos
-float total = 0; // Total de las que llevamos
-float media = 0; // Media de las medidas
-bool primeraMedia = false; // Para saber que ya hemos calculado por lo menos una
+int lecturaActual = 0;       // Lectura por la que vamos
+float total = 0;             // Total de las que llevamos
+float media = 0;             // Media de las medidas
+bool primeraMedia = false;   // Para saber que ya hemos calculado por lo menos una
 
 void setup()
 {
-  Serial.begin(9600);  // Start serial communication
+  Serial.begin(9600); // Start serial communication
   // Mandamos comandos para toma de temperatura a los sensores
-  scale.begin(hx711_data_pin, hx711_clock_pin);  // Initialize the HX711 library
-  scale.set_scale(439430.25);  // Set the scale factor
-  Serial.println(scale.read());   // Read the current value
-  scale.tare(0);  // Tare the scale
-  Serial.println(scale.get_units(20),2);  // Read the current value in grams
-  pinMode(bombpwm1, OUTPUT);  // Set the PWM1 pin as an output
-  pinMode(bombpwm2, OUTPUT);  // Set the PWM2 pin as an output
-  analogWrite(bombpwm1, 63);  // Set the PWM1 pin to 63
-  analogWrite(bombpwm2, 63);    // Set the PWM2 pin to 63
+  scale.begin(hx711_data_pin, hx711_clock_pin); // Initialize the HX711 library
+  scale.set_scale(439430.25);                   // Set the scale factor
+  Serial.println(scale.read());                 // Read the current value
+  scale.tare(0);                                // Tare the scale
+  Serial.println(scale.get_units(20), 2);       // Read the current value in grams
+  pinMode(bombpwm1, OUTPUT);                    // Set the PWM1 pin as an output
+  pinMode(bombpwm2, OUTPUT);                    // Set the PWM2 pin as an output
+  analogWrite(bombpwm1, 63);                    // Set the PWM1 pin to 63
+  analogWrite(bombpwm2, 63);                    // Set the PWM2 pin to 63
   // Ponemos el pin Trig en modo salida
   pinMode(PinTrig, OUTPUT);
   // Ponemos el pin Echo en modo entrada
@@ -46,72 +46,71 @@ void setup()
   }
 }
 
-float dist (){
-// Eliminamos la última medida
+void iniciarTrigger()
+{
+  // Ponemos el Triiger en estado bajo y esperamos 2 ms
+  digitalWrite(PinTrig, LOW);
+  delayMicroseconds(2);
+
+  // Ponemos el pin Trigger a estado alto y esperamos 10 ms
+  digitalWrite(PinTrig, HIGH);
+  delayMicroseconds(10);
+
+  // Comenzamos poniendo el pin Trigger en estado bajo
+  digitalWrite(PinTrig, LOW);
+}
+
+float dist()
+{
+  // Eliminamos la última medida
   total = total - lecturas[lecturaActual];
- 
+
   iniciarTrigger();
- 
+
   // La función pulseIn obtiene el tiempo que tarda en cambiar entre estados, en este caso a HIGH
   unsigned long tiempo = pulseIn(PinEcho, HIGH);
- 
+
   // Obtenemos la distancia en cm, hay que convertir el tiempo en segudos ya que está en microsegundos
   // por eso se multiplica por 0.000001
   float distancia = tiempo * 0.000001 * VelSon / 2.0;
- 
+
   // Almacenamos la distancia en el array
   lecturas[lecturaActual] = distancia;
- 
+
   // Añadimos la lectura al total
   total = total + lecturas[lecturaActual];
- 
+
   // Avanzamos a la siguiente posición del array
   lecturaActual = lecturaActual + 1;
- 
+
   // Comprobamos si hemos llegado al final del array
   if (lecturaActual >= numLecturas)
   {
     primeraMedia = true;
     lecturaActual = 0;
   }
- 
+
   // Calculamos la media
   media = total / numLecturas;
- 
+
   // Solo mostramos si hemos calculado por lo menos una media
   if (primeraMedia)
   {
     float distanciaLleno = distanciaVacio - media;
-    
+
     Serial.print(media);
     Serial.println(" cm");
-    
-    
   }
   return media;
-}
-
-void iniciarTrigger()
-{
-  // Ponemos el Triiger en estado bajo y esperamos 2 ms
-  digitalWrite(PinTrig, LOW);
-  delayMicroseconds(2);
- 
-  // Ponemos el pin Trigger a estado alto y esperamos 10 ms
-  digitalWrite(PinTrig, HIGH);
-  delayMicroseconds(10);
- 
-  // Comenzamos poniendo el pin Trigger en estado bajo
-  digitalWrite(PinTrig, LOW);
 }
 
 
 
 void loop()
 {
-  String T1 = String(scale.get_units(20),2);    // obtener t1P
-  Serial.println(scale.read()); // obtener t1
-  String T2 = String(media);                     // obtener t2P
+  String T1 = String(scale.get_units(20), 2); // obtener t1P
+  Serial.println(scale.read());               // obtener t1
+  String T2 = String(dist());                  // obtener t2P
 
   Serial.print(T1 + "A" + T2);
   delay(1000);

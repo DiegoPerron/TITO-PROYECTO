3af14565e685e604833c72076a4f008f5fd9a222
DiegoPerron <2020348121@uteq.edu.mx>
2022-11-30 15:19:59 -0600

    arreglao

--- TITO PROYECTO/src/main.cpp
+++ TITO PROYECTO/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 #include <Q2HX711.h>
 
-const byte hx711_data_pin = A2;
-const byte hx711_clock_pin = A3;
-char caso ; 
+const byte hx711_data_pin = A1;
+const byte hx711_clock_pin = A0;
+const byte bombpwm1 = 3;
+char caso;
+
+void setup()
+{
+  Serial.begin(9600);
+  // Mandamos comandos para toma de temperatura a los sensores
+  Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
+  pinMode(bombpwm1, OUTPUT);
+}
+
 void loop()
 {
 
-  // Mandamos comandos para toma de temperatura a los sensores
-  Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
-
   // Leemos y mostramos los datos de los sensores DS18B20
-  String T1 = String(hx711.read()/100.0); // obtener t1
-  String T2 = String(); // obtener t2P
+  String T1 = String(hx711.read() / 100.0); // obtener t1
+  String T2 = String();                     // obtener t2P
 
   Serial.print(T1 + "A" + T2);
   delay(100);

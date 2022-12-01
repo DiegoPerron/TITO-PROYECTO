60f840986b5feb290e74870b2c57c7c0ce1ce273
DiegoPerron <2020348121@uteq.edu.mx>
2022-11-30 20:36:08 -0600

    quzas queda la seleccion

--- TITO PROYECTO/src/main.cpp
+++ TITO PROYECTO/src/main.cpp
@@ -3,8 +3,8 @@
 
 const byte hx711_data_pin = A1;  // DOUT
 const byte hx711_clock_pin = A0; // CLK
-const byte bomba1 = 2;           // bomba que mete agua
-const byte bomba2 = 3;           // bomba que saca agua
+const byte bomba1 = 2;     //bomba que mete agua
+const byte bomba2 = 3;  //bomba que saca agua
 const byte bombpwm1 = 9;         // PWM1
 const byte bombpwm2 = 6;         // PWM2
 HX711 scale;                     // Create an instance of the HX711 class
@@ -25,20 +25,21 @@ float media = 0;             // Media de las medidas
 bool primeraMedia = false;   // Para saber que ya hemos calculado por lo menos una
 float distanciaLleno = 0;    // Distancia lleno
 float mediareal = 0;         // Media real
+bool state = false;          // Estado de la medicion
 
 void setup()
 {
   Serial.begin(9600); // Start serial communication
   // Mandamos comandos para toma de temperatura a los sensores
   scale.begin(hx711_data_pin, hx711_clock_pin); // Initialize the HX711 library
-  scale.set_scale(219715.125);                  // Set the scale factor
+  scale.set_scale(219715.125);                   // Set the scale factor
   Serial.println(scale.read());                 // Read the current value
   scale.tare(0);                                // Tare the scale
   Serial.println(scale.get_units(20), 2);       // Read the current value in grams
   pinMode(bombpwm1, OUTPUT);                    // Set the PWM1 pin as an output
   pinMode(bombpwm2, OUTPUT);                    // Set the PWM2 pin as an output
-  analogWrite(bombpwm1, 120);                   // Set the PWM1 pin to 63
-  analogWrite(bombpwm2, 120);                   // Set the PWM2 pin to 63
+  analogWrite(bombpwm1, 120);                    // Set the PWM1 pin to 63
+  analogWrite(bombpwm2, 120);                    // Set the PWM2 pin to 63
   // Ponemos el pin Trig en modo salida
   pinMode(PinTrig, OUTPUT);
   // Ponemos el pin Echo en modo entrada
@@ -64,52 +65,53 @@ void iniciarTrigger()
   digitalWrite(PinTrig, LOW);
 }
 
-float dist()
-{
-
+float dist(){
+  
   // Eliminamos la última medida
   total = total - lecturas[lecturaActual];
-
+ 
   iniciarTrigger();
-
+ 
   // La función pulseIn obtiene el tiempo que tarda en cambiar entre estados, en este caso a HIGH
   unsigned long tiempo = pulseIn(PinEcho, HIGH);
-
+ 
   // Obtenemos la distancia en cm, hay que convertir el tiempo en segudos ya que está en microsegundos
   // por eso se multiplica por 0.000001
   float distancia = tiempo * 0.000001 * VelSon / 2.0;
-
+ 
   // Almacenamos la distancia en el array
   lecturas[lecturaActual] = distancia;
-
+ 
   // Añadimos la lectura al total
   total = total + lecturas[lecturaActual];
-
+ 
   // Avanzamos a la siguiente posición del array
   lecturaActual = lecturaActual + 1;
-
+ 
   // Comprobamos si hemos llegado al final del array
   if (lecturaActual >= numLecturas)
   {
     primeraMedia = true;
     lecturaActual = 0;
   }
-
+ 
   // Calculamos la media
   media = total / numLecturas;
-
+ 
   // Solo mostramos si hemos calculado por lo menos una media
   if (primeraMedia)
   {
     distanciaLleno = distanciaVacio - media;
+    
+    
+    
   }
   delay(100);
   return distanciaLleno;
 }
 
-float lectura()
-{
-  for (int i = 0; i < 10; i++)
+float lectura(){
+    for (int i = 0; i < 10; i++)
   {
     mediareal = mediareal + dist();
   }
@@ -117,79 +119,61 @@ float lectura()
   return mediareal;
 }
 
+
+
 void loop()
 {
+  if (state==false){
+  String T2 = String(lectura());                  // obtener t2P
+  String T1 = String(0);  //obtener t1P
+
+
+  }
+  if (state==true){
+  String T2 = String(0);                  // obtener t2P
+  String T1 = String(scale.get_units(20), 2);  //obtener t1P
+  }
+
+
+
+
+
+  Serial.println(T1 + "A" + T2);
+
 
   if (Serial.available())
   {
     caso = Serial.read();
-    if (caso == 'AX')
+    if (caso == 'A')
     {
       digitalWrite(bomba1, HIGH);
       digitalWrite(bomba2, LOW);
-      String T2 = String(lectura()); // obtener t2P
-      String T1 = String(0);         // obtener t1P
-      Serial.println(T1 + "A" + T2);
     } // sasoa
-    if (caso == 'BX')
+    if (caso == 'B')
     {
       digitalWrite(bomba1, LOW);
       digitalWrite(bomba2, LOW);
-      String T2 = String(lectura()); // obtener t2P
-      String T1 = String(0);         // obtener t1P
-      Serial.println(T1 + "A" + T2);
-    } // casob
-    if (caso == 'CX')
+    }  // casob
+    if (caso == 'C')
     {
       digitalWrite(bomba1, LOW);
       digitalWrite(bomba2, HIGH);
-      String T2 = String(lectura()); // obtener t2P
-      String T1 = String(0);         // obtener t1P
-      Serial.println(T1 + "A" + T2);
     } // casoc
 
-    if (caso == 'DX')
+    if (caso == 'D')
     {
       digitalWrite(bomba1, LOW);
       digitalWrite(bomba2, LOW);
-      String T2 = String(lectura()); // obtener t2P
-      String T1 = String(0);         // obtener t1P
-      Serial.println(T1 + "A" + T2);
     } // casoD
-    caso = Serial.read();
-    if (caso == 'AZ')
+    if (caso == "X")
     {
-      digitalWrite(bomba1, HIGH);
-      digitalWrite(bomba2, LOW);
-      String T2 = String(0);                      // obtener t2P
-      String T1 = String(scale.get_units(20), 2); // obtener t1P
-      Serial.println(T1 + "A" + T2);
-    } // sasoa
-    if (caso == 'BZ')
+      state = true;
+    }
+    if (caso == "Y")
     {
-      digitalWrite(bomba1, LOW);
-      digitalWrite(bomba2, LOW);
-      String T2 = String(0);                      // obtener t2P
-      String T1 = String(scale.get_units(20), 2); // obtener t1P
-      Serial.println(T1 + "A" + T2);
-    } // casob
-    if (caso == 'CZ')
-    {
-      digitalWrite(bomba1, LOW);
-      digitalWrite(bomba2, HIGH);
-      String T2 = String(0);                      // obtener t2P
-      String T1 = String(scale.get_units(20), 2); // obtener t1P
-      Serial.println(T1 + "A" + T2);
-    } // casoc
-
-    if (caso == 'DZ')
-    {
-      digitalWrite(bomba1, LOW);
-      digitalWrite(bomba2, LOW);
-      String T2 = String(0);                      // obtener t2P
-      String T1 = String(scale.get_units(20), 2); // obtener t1P
-      Serial.println(T1 + "A" + T2);
-    } // casoD
+      state = false;
+    }
+    
   }   // casos
 
 } // void
